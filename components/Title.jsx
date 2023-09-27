32a6d6c7c55f73b71805ddf63ae719df68ff7fff
Ebram Mekhail <ebrammekhail@gmail.com>
2023-09-27 01:47:23 -0400

    Added Button For quotes

---
+++ components/Title.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Title() {
+  return (
+    <div className="pb-32 text-5xl ">
+      <h1 className="bg-gradient-to-r from-purple-500 to-pink-500 inline-block text-transparent bg-clip-text">
+        IS322: Project Github Repo Setup
+      </h1>
+    </div>
+  );
+}
+
+export default Title;
